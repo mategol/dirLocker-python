bf5ec4ce40204cc20e2ddfb2cf9afe99090d4924
Mateusz Gołembowski <44233157+mategol@users.noreply.github.com>
2022-06-28 09:44:49 -0500

    Added configuration checking

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import configparser
+import os
 
+software_version = 1.0
+default_secured_directory = 'secured_dir'
+
+configuration = configparser.ConfigParser()
+if 'configuration.ini' not in os.listdir('.'):
+    configuration['CONFIGURATION'] = {'secured_directory': default_secured_directory,
+                                      'locale': 'en'}
+    with open('configuration.ini', 'w') as configuration_file:
+        configuration.write(configuration_file)
+else:
+    configuration.read('configuration.ini')
+    locale = configuration['CONFIGURATION']['locale']
+    secured_directory = configuration['CONFIGURATION']['secured_directory']
