938a77a9f3a4249f160d5fc6b2261dcd1cf94d2b
Mateusz Gołembowski <44233157+mategol@users.noreply.github.com>
2022-06-28 16:43:43 -0500

    Added program startup in registry

--- resources/registry_manager.py
+++ resources/registry_manager.py
@@ -1,1 +1,8 @@
+import winreg
+import sys
 
+def registry_append():
+    winreg.CreateKey(winreg.HKEY_CURRENT_USER, 'Software\\Microsoft\\Windows\\CurrentVersion\\Run')
+    registry_key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, 'Software\\Microsoft\\Windows\\CurrentVersion\\Run', 0, winreg.KEY_WRITE)
+    winreg.SetValueEx(registry_key, 'dirLocker', 0, winreg.REG_SZ, sys.argv[0])
+    winreg.CloseKey(registry_key)
