6fed02d677b3eda4f356c19c26460854dcf8639d
Mateusz Gołembowski <44233157+mategol@users.noreply.github.com>
2022-06-28 16:13:33 -0500

    Configuration will be checked every 10 seconds

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import configparser
 import os
+import time
+import threading
 
 software_version = 1.0
 default_secured_directory = 'secured_dir'
 
-configuration = configparser.ConfigParser()
-if 'configuration.ini' not in os.listdir('.'):
-    configuration['CONFIGURATION'] = {'secured_directory': default_secured_directory,
-                                      'locale': 'en'}
-    with open('configuration.ini', 'w') as configuration_file:
-        configuration.write(configuration_file)
-else:
-    configuration.read('configuration.ini')
-    locale = configuration['CONFIGURATION']['locale']
-    secured_directory = configuration['CONFIGURATION']['secured_directory']
+def update_configuration():
+    global locale, secured_directory, configuration
+    while True:
+        time.sleep(10)
+        configuration = configparser.ConfigParser()
+        if 'configuration.ini' not in os.listdir('.'):
+            configuration['CONFIGURATION'] = {'secured_directory': default_secured_directory,
+                                              'locale': 'en'}
+            with open('configuration.ini', 'w') as configuration_file:
+                configuration.write(configuration_file)
+        else:
+            configuration.read('configuration.ini')
+            locale = configuration['CONFIGURATION']['locale']
+            secured_directory = configuration['CONFIGURATION']['secured_directory']
+
+threading.Thread(target=update_configuration).start()
