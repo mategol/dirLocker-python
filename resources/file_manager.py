2d4c5564fca1a9aed1935ed9ce850020f593a4b6
Mateusz Gołembowski <44233157+mategol@users.noreply.github.com>
2022-06-29 03:43:42 -0500

    Added file shortcut creation

--- resources/file_manager.py
+++ resources/file_manager.py
@@ -1,1 +1,11 @@
+from win32com.client import Dispatch
 
+def create_shortcut(path, target='', wDir='', icon=''):    
+    shell = Dispatch('WScript.Shell')
+    shortcut = shell.CreateShortCut(path)
+    shortcut.Targetpath = target[0]
+    shortcut.Arguments = target[1]
+    shortcut.WorkingDirectory = wDir
+    if icon != '':
+        shortcut.IconLocation = icon
+    shortcut.save()
