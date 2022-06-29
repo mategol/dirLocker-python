b7d6fee641cf5d48138e64d6c89a49dd3bab5794
Mateusz Gołembowski <44233157+mategol@users.noreply.github.com>
2022-06-29 04:39:12 -0500

    Created filename generating function

--- resources/file_manager.py
+++ resources/file_manager.py
@@ -1,5 +1,27 @@
 from win32com.client import Dispatch
 import os
+from resources.configuration_manager import *
+
+def generate_name(input_name, is_directory):
+    extension = input_name.split('.')[-1]
+    file_name = input_name.split('.')[:-1]
+
+    if is_directory:
+        additional_extension = '.directory'
+    else:
+        additional_extension = ''
+
+    list_of_encryption_directory = os.listdir(get_secured_directory())
+    if input_name + additional_extension + '.dlock' in list_of_encryption_directory:
+        loop_number = 0
+        while True:
+            loop_number += 1
+            if file_name + '_' + str(loop_number) + extension + additional_extension + '.dlock' not in list_of_encryption_directory:
+                return file_name + '_' + str(loop_number) + extension + additional_extension
+            else:
+                continue
+    else:
+        return input_name + additional_extension
 
 def create_shortcut(path, target='', wDir='', icon=''):    
     shell = Dispatch('WScript.Shell')
