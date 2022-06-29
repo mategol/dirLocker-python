8063096cc8d325c4a548a0f9cdeddf9a7a383e5b
Mateusz Gołembowski <44233157+mategol@users.noreply.github.com>
2022-06-29 04:31:49 -0500

    Added function for getting secured directory path

--- resources/configuration_manager.py
+++ resources/configuration_manager.py
@@ -3,10 +3,17 @@ import time
 import os
 import threading
 
+def get_secured_directory():
+    global configuration
+    return configuration['CONFIGURATION']['secured_directory']
+
+def get_locale():
+    global configuration
+    return configuration['CONFIGURATION']['locale']
+
 def update_configuration():
     global locale, secured_directory, configuration
     while True:
-        time.sleep(10)
         configuration = configparser.ConfigParser()
         if 'configuration.ini' not in os.listdir('.'):
             configuration['CONFIGURATION'] = {'secured_directory': default_secured_directory,
@@ -17,8 +24,9 @@ def update_configuration():
             configuration.read('configuration.ini')
             locale = configuration['CONFIGURATION']['locale']
             secured_directory = configuration['CONFIGURATION']['secured_directory']
+        time.sleep(10)
 
 def configuration_initialize(default_directory):
-    global default_secured_directory
+    global default_secured_directory, configuration
     default_secured_directory = default_directory
     threading.Thread(target=update_configuration).start()
